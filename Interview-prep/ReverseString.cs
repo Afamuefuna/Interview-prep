4b195922459c87f4fcac963d92a8d36d818d7857
Simon Abugu <abugulivinusbro@gmail.com>
2021-10-22 21:54:14 +0100

    Reverse a string

---
+++ Interview-prep/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class ReverseString
+{
+	public ReverseString(string stringToReverse)
+	{
+		char[] charArray = stringToReverse.ToCharArray();
+		Array.Reverse(charArray);
+		return new string(charArray);
+	}
+}
