1bd8e2d022b970e98bb83923a5fa63cd0fec78a5
Simon Abugu <abugulivinusbro@gmail.com>
2021-10-22 19:04:37 +0100

    FizzBuzz Problem

--- Interview-prep/Palindrome.cs
+++ Interview-prep/Palindrome.cs
@@ -4,5 +4,6 @@ public class Palindrome
 {
 	public Palindrome()
 	{
+
 	}
 }
--- Interview-prep/Program.cs
+++ Interview-prep/Program.cs
@@ -8,9 +8,32 @@ namespace Interview_prep
 {
     class Program
     {
+        public void FizzBuzz()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i.ToString() + " FizzBuzz");
+                }
+                else
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i.ToString() + " is Fizz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i.ToString() + " is Buzz");
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-
+            Program program = new Program();
+            program.FizzBuzz();
         }
     }
 }
