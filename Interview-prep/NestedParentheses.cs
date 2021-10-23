6070dbcae81fe8544365b99c942e1bbcadb6695e
Simon Abugu <abugulivinusbro@gmail.com>
2021-10-23 02:13:10 +0100

    Nested parentheses

---
+++ Interview-prep/NestedParentheses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class NestedParentheses
+{
+	public static Boolean NestedParentheses(string s)
+	{
+		int count = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+			char ch = s[i];
+			if(ch == "(")
+            {
+				++count;
+            }else if(ch == ")")
+            {
+                --count;
+                if (count < 0) return false;
+            }
+        }
+        return count == 0;
+	}
+}
