e32100b7f87498dd3914eef77a9d76833321dd87
Simon Abugu <abugulivinusbro@gmail.com>
2021-10-24 22:42:33 +0100

    Doubly Linkedlist

--- Interview-prep/LinkedList.cs
+++ Interview-prep/LinkedList.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 
+//Singly linked list
 internal class Node
 {
-    //Singly linked list
-
     internal int data;
     internal Node next;
     public Node(int d)
     {
         data = d;
         next = null;
+    }
+
+    
+}
+
+//Doubly linked list
+internal class DNode(){
+
+    internal int data;
+    internal DNode prev;
+    internal DNode next;
+    public DNode(int d)
+    {
+        data = d;
+        next = null;
+        prev = null;
     }
 }
